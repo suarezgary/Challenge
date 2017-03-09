e0290c4d8e9f1c6a2b87eae32501d398dbbe3676
CA951FC <gsuarez@tecnosoftware.com>
2017-03-09 16:50:11 -0300

    Request Adapted to GetCategories

--- GetCategoriesReq.py
+++ GetCategoriesReq.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'https://api.sandbox.ebay.com/ws/api.dll'
-headers = {'X-EBAY-API-CALL-NAME':'GeteBayOfficialTime',
+headers = {'X-EBAY-API-CALL-NAME':'GetCategories',
 'X-EBAY-API-APP-NAME':'EchoBay62-5538-466c-b43b-662768d6841',
 'X-EBAY-API-CERT-NAME':'00dd08ab-2082-4e3c-9518-5f4298f296db',
 'X-EBAY-API-DEV-NAME':'16a26b1b-26cf-442d-906d-597b60c41c19',
@@ -9,11 +9,12 @@ headers = {'X-EBAY-API-CALL-NAME':'GeteBayOfficialTime',
 'X-EBAY-API-COMPATIBILITY-LEVEL':'861'}
 
 payload = '''<?xml version="1.0" encoding="utf-8"?>
-<GeteBayOfficialTimeRequest xmlns="urn:ebay:apis:eBLBaseComponents">
+<GetCategoriesRequest xmlns="urn:ebay:apis:eBLBaseComponents">
   <RequesterCredentials>
     <eBayAuthToken>AgAAAA**AQAAAA**aAAAAA**PMIhVg**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6wFk4GhCpaCpQWdj6x9nY+seQ**L0MCAA**AAMAAA**IahulXaONmBwi/Pzhx0hMqjHhVAz9/qrFLIkfGH5wFH8Fjwj8+H5FN4NvzHaDPFf0qQtPMFUaOXHpJ8M7c2OFDJ7LBK2+JVlTi5gh0r+g4I0wpNYLtXnq0zgeS8N6KPl8SQiGLr05e9TgLRdxpxkFVS/VTVxejPkXVMs/LCN/Jr1BXrOUmVkT/4Euuo6slGyjaUtoqYMQnmBcRsK4xLiBBDtiow6YHReCJ0u8oxBeVZo3S2jABoDDO9DHLt7cS73vPQyIbdm2nP4w4BvtFsFVuaq6uMJAbFBP4F/v/U5JBZUPMElLrkXLMlkQFAB3aPvqZvpGw7S8SgL7d2s0GxnhVSbh4QAqQrQA0guK7OSqNoV+vl+N0mO24Aw8whOFxQXapTSRcy8wI8IZJynn6vaMpBl5cOuwPgdLMnnE+JvmFtQFrxa+k/9PRoVFm+13iGoue4bMY67Zcbcx65PXDXktoM3V+sSzSGhg5M+R6MXhxlN3xYfwq8vhBQfRlbIq+SU2FhicEmTRHrpaMCk4Gtn8CKNGpEr1GiNlVtbfjQn0LXPp7aYGgh0A/b8ayE1LUMKne02JBQgancNgMGjByCIemi8Dd1oU1NkgICFDbHapDhATTzgKpulY02BToW7kkrt3y6BoESruIGxTjzSVnSAbGk1vfYsQRwjtF6BNbr5Goi52M510DizujC+s+lSpK4P0+RF9AwtrUpVVu2PP8taB6FEpe39h8RWTM+aRDnDny/v7wA/GkkvfGhiioCN0z48</eBayAuthToken>
   </RequesterCredentials>
-</GeteBayOfficialTimeRequest>'''
+  <DetailLevel>ReturnAll</DetailLevel>
+</GetCategoriesRequest>'''
 
 r = requests.post(url, data=payload, headers=headers )
 
